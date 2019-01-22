eae7b91f780255a24ec47a8eba93e7c8c1afc8ba
Jailson Silva <jjsj2@cin.ufpe.br>
2019-01-22 06:08:09 -0300

    connecting service with database

--- app/src/main/java/com/example/jailson/showupartist/service/ArtistService.java
+++ app/src/main/java/com/example/jailson/showupartist/service/ArtistService.java
@@ -1,16 +1,19 @@
 package com.example.jailson.showupartist.service;
 
+import android.content.Context;
+
+import com.example.jailson.showupartist.data.DataBaseAdapter;
 import com.example.jailson.showupartist.entity.Artist;
 
 public class ArtistService {
 
+    private DataBaseAdapter dataBaseAdapter;
+
     private Artist artist;
 
-    public ArtistService(){
+    public ArtistService(Context context){
 
-        this.artist = new Artist();
-        this.artist.setEmail("jailsonjosejr@gmail.com");
-        this.artist.setPassword("1234");
+        this.dataBaseAdapter = new DataBaseAdapter(context);
     }
 
     public Artist loginArtist(String email, String password){
