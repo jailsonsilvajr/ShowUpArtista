e7eed410849a3755ef9b39ce0f639491b2360b32
Jailson Silva <jjsj2@cin.ufpe.br>
2019-01-22 05:58:04 -0300

    Create class DataBaseAdapter

---
+++ app/src/main/java/com/example/jailson/showupartist/data/DataBaseAdapter.java
@@ -0,0 +1,35 @@
+package com.example.jailson.showupartist.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseAdapter extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    protected static final String DATABASE_NAME = "ShowUpArtist.DB";
+
+    public DataBaseAdapter(Context context) {
+
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql = "CREATE TABLE artist " +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "name TEXT, " +
+                "email TEXT, " +
+                "password NUMBER)";
+
+        db.execSQL(sql);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        //Falta criar método para backup!!
+        String sql = "DROP TABLE IF EXISTS artist";
+        db.execSQL(sql);
+        onCreate(db);
+    }
+}
